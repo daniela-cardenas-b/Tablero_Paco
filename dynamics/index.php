dc976077bf35d9dc043dd5908bbb2ade102607af
daniela-cardenas-b <daniela.cardenas@comunidad.unam.mx>
2022-05-04 12:22:11 -0500

    Resolviendo el conflicto

--- dynamics/index.php
+++ dynamics/index.php
@@ -13,19 +13,19 @@
 
     echo "<table border='2'>";
 
-    for ($i = 0; $i < $size; $i++) {
+    for ($i = 0; $i < $size; $i++) 
+    {
         echo "<tr>";
 
-    for ($j = 0; $j < $size; $j++) {
-        if ($i % 2 === $j % 2) {
-            echo "<td><img src= '../assets/blanco.png'></td>";
-        } else {
-            echo "<td><img src= '../assets/negro.png'></td>";
+        for ($j = 0; $j < $size; $j++) {
+            if ($i % 2 === $j % 2) {
+                echo "<td><img src= '../assets/blanco.png'></td>";
+            } else {
+                echo "<td><img src= '../assets/negro.png'></td>";
+            }
         }
-
         echo "</tr>";
     }
-
     echo "</table>";
 
     ?>
