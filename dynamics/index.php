12ed7300f440a9aedb6d71cb7d35e5e822e7781d
daniela-cardenas-b <daniela.cardenas@comunidad.unam.mx>
2022-05-04 11:46:07 -0500

    Maquetado HTML en el PHP

--- dynamics/index.php
+++ dynamics/index.php
@@ -1,23 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-$size = $_POST["size"];
+    $size = $_POST["size"];
 
-echo "<table border='2'>";
+    echo "<table border='2'>";
 
-for ($i = 0; $i < $size; $i++) {
-    echo "<tr>";
+    for ($i = 0; $i < $size; $i++) {
+        echo "<tr>";
 
-    for ($j = 0; $j < $size; $j++) {
-        if ($i % 2 === $j % 2) {
-            echo "<td>+</td>";
-        } else {
-            echo "<td>-</td>";
+        for ($j = 0; $j < $size; $j++) {
+            if ($i % 2 === $j % 2) {
+                echo "<td>+</td>";
+            } else {
+                echo "<td>-</td>";
+            }
         }
+
+        echo "</tr>";
     }
 
-    echo "</tr>";
-}
+    echo "</table>";
 
-echo "</table>";
+    ?>
 
-?>
+</body>
+</html>
